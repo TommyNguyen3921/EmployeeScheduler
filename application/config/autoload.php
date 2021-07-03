8d40fd7e99721872ec872448c9770661e5e50794
TommyNguyen3921 <55663532+TommyNguyen3921@users.noreply.github.com>
2021-07-03 19:42:18 -0400

    tes4

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array("report_model");
+$autoload['model'] = array("Report_model");
